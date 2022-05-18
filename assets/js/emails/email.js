70c5b6ec393cf6cf615a6d460d8938c36ce63959
Sharona7090 <86896028+Sharona7090@users.noreply.github.com>
2022-05-18 13:08:47 -0500

    add startemail function to pick which client/email template to use

--- assets/js/emails/email.js
+++ assets/js/emails/email.js
@@ -1,31 +1,4 @@
-function clicknext(){
-  document.getElementById("emailcase").style.display="none"
- document.getElementById("emailform").style.display="block"
-
- var globalMatter = document.getElementById("emailMatter").value
- document.getElementById("emailbuttonscardcase2").innerHTML = globalMatter;
-//   document.getElementById("emailcase").style.display="none"
-//   // document.getElementById("emailform2").style.display="block"
-//   if (globalMatter == "001.001") {
-//     document.getElementById("emailform1").style.display="block"
-//   }
-//   else if (globalMatter == "002.002") {
-//     document.getElementById("emailform2").style.display="block"
-//   } 
-// else  if (globalMatter == "002.003") {
-//     document.getElementById("emailform3").style.display="block"
-//   } else { document.getElementById("emailform4").style.display="block"
-// }
-  // document.getElementById("emailCardtitle").innerHTML="Email For "+ globalMatter
+function startemail(client, email, casedata){
+// if (client == "Amazon" || )
+console.log(client, email, casedata)
 }
-function clickback(){
-  document.getElementById("emailcase").style.display="block"
-  document.getElementById("emailform").style.display="none"
-
-  // document.getElementById("emailform1").style.display="none"
-  // document.getElementById("emailform2").style.display="none"
-  // document.getElementById("emailform3").style.display="none"
-  // document.getElementById("emailform4").style.display="none"
-}
-
-///////////////////////////////////////////////////////////////////////////////
