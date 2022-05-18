01c7f8a3510a8509a1b450d98077a829f679243a
Sharona7090 <86896028+Sharona7090@users.noreply.github.com>
2022-05-18 15:36:07 -0500

    debug claim expand/contract

--- assets/js/applications/application.js
+++ assets/js/applications/application.js
@@ -497,8 +497,7 @@ function funcClaims(){
 
 function claimExpand(){
 	document.getElementById('appaside').style="display: none;"
-	document.getElementsByClassName('Group')[0].style="display: none;"
-	// document.getElementById('kt_wrapper').style="padding:0px 15px;"
+	document.getElementsByClassName('Group')[0].style.display="none"
   document.getElementById('container').style.padding="0px 50px 0px;"
   document.getElementById('claimsDiv').style.gridTemplateRows="50px calc(100vh - 50px)"
 	document.getElementById('claimsToggle').onclick=function() {claimContract()};
@@ -507,10 +506,7 @@ function claimExpand(){
 
 function claimContract(){
 	document.getElementById('appaside').style="border-radius:0; background-color:white; padding:50px"
-  document.getElementsByClassName('Group')[0].style="display: flex;"
-
-	// document.getElementById('kt_header').style="display: inherit;"
-	// document.getElementById('kt_wrapper').style="padding:"
+  document.getElementsByClassName('Group')[0].style.display="flex;"
 	document.getElementById('claimsToggle').onclick=function() {claimExpand()};
 	document.getElementById('claimsToggle').innerHTML='<i class="fas fa-expand" style="font-size:1.75rem"></i>';
   document.getElementById('claimsDiv').style.gridTemplateRows="50px calc(100vh - 130px)"
