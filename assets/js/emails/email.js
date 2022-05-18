0f399e0f1aed6f8b77966d0351d8b4c46daaac77
Sharona7090 <86896028+Sharona7090@users.noreply.github.com>
2022-05-18 13:29:51 -0500

    email javascript

--- assets/js/emails/email.js
+++ assets/js/emails/email.js
@@ -1,4 +1,5 @@
 function startemail(client, email, casedata){
 // if (client == "Amazon" || )
 console.log(client, email, casedata)
+funcNfoaRec(casedata)
 }
--- assets/js/emails/emailtemplates.js
+++ assets/js/emails/emailtemplates.js
@@ -1,38 +1,31 @@
-  function funcNfoaRec(){
-  var textbox = document.getElementById("anotes");
-  console.log(textbox)
+function funcNfoaRec(casedata){
   var actionFull = "Non-Final Office Action Recommendation"
-  var clientno = "4723-US"
-  var matter = "14-090241"
-  console.log(matter)
-  var title = "Multi-Modal Haptic Feedback Touch Screen"
-  var file = "June 2, 2021"
-  var ihc = "John"
+  var clientno = casedata.clientid
+  var matter = casedata.firmid
+  var title = casedata.title
+  var file = casedata.filingdate
+  // var ihc = "John"
   var to = "To: "
   var cc = "\r\Cc: "
   var subject = "\r\Subject: " + clientno + " / " + matter + " - " + actionFull + " (privileged and confidential)" + "\r\X-Unsent: 1 \r\Content-Type: text/html";
   var body = "\n\r<html><body style='font-size: 11pt; font-family:\"Calibri\",\"sans-serif\"';><table style=\"font-family:calibri; font-size: 11pt; border:0px none; border-collapse:collapse; padding:0px;\"><tr style=\"border:0px none; border-collapse:collapse; padding:0px;\"><td style=\"font-family:calibri; font-size: 10.5pt; border:0px none; border-collapse:collapse;padding:0px;\"><b>NO ACTION REQUIRED &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></td><td style=\"font-family:calibri; font-size: 11pt; border:0px none;border-collapse:collapse;padding:0px;\"><b>  </b></td></tr><tr style=\"border:0px none;border-collapse:collapse;padding:0px;\"><td style=\"font-family:calibri; font-size: 11pt; border:0px none;border-collapse:collapse;padding:0px;\">Amazon Tracking No.:&nbsp;&nbsp;</td><td style=\"font-family:calibri; font-size: 11pt;border:0px none;border-collapse:collapse;	padding:0px;\">" + clientno + "</td></tr><tr style=\"border:0px none; border-collapse:collapse;padding:0px;\"><td style=\"font-family:calibri; font-size: 11pt; border:0px none;border-collapse:collapse;padding:0px;\">OC Tracking No.:&nbsp;&nbsp;</td><td style=\"font-family:calibri; font-size: 11pt; border:0px none;border-collapse:collapse;padding:0px;\">" + matter + "</td></tr><tr style=\"border:0px none;border-collapse:collapse;padding:0px;\"><td style=\"font-family:calibri; font-size: 11pt; border:0px none;border-collapse:collapse;padding:0px;\">Matter Name:&nbsp;&nbsp;</td><td style=\"font-family:calibri; font-size: 11pt; border:0px none; border-collapse:collapse;padding:0px;\">" +  title + "</td></tr><tr style=\"border:0px none;	border-collapse:collapse;	padding:0px;\"><td style=\"font-family:calibri; font-size: 11pt; border:0px none;	border-collapse:collapse;padding:0px;\">Response Due Date:&nbsp;&nbsp;</td><td style=\"font-family:calibri; font-size: 11pt; border:0px none;		border-collapse:collapse;padding:0px;\">" + file + "</td></tr></table><br>"
   /////    Body    ///////
   + "Dear "+ihc+",<br><br>We have received a Non-Final Office Action in the above-referenced patent application. Attached are copies of the Non-Final Office Action for your review. Our summary of the rejections and recommendation for response are as follows: <br><br><b>Do you recommend an Examiner interview?</b><br>[<i>Yes/No. Include a brief explanation.</i>]<br><br><b>Summary of rejected claims, cited art, and plan to overcome rejection:</b><br>[<i>Insert a brief summary of the status of the application, including number of RCEs filed, relevant materials, and your recommendation/response strategy.</i>]<br>" +
-
     /////    Signature    ///////
-"<br>Best,<br> Pat </body></html>" +  "<html><body lang=EN-US link=blue vlink=purple><div class=WordSection1><p class=MsoNormal style='margin-bottom:6.0pt'><b><span style='font-size:10.0pt;font-family:\"Arial\",\"sans-serif\";mso-fareast-language:ZH-CN'>Pat Lawyerton</span></b><b><span style='font-size:8.0pt;font-family:\"Arial\",\"sans-serif\";text-transform:uppercase;letter-spacing:1.0pt'><br></span></b><span style='font-size:8.0pt;font-family:\"Arial\",\"sans-serif\";color:#606060;mso-fareast-language:ZH-CN'>Senior Associate</span><span style='font-size:8.0pt;font-family:\"Arial\",\"sans-serif\"'><o:p></o:p></span></p><table class=MsoNormalTable border=0 cellspacing=0 cellpadding=0 style='border-collapse:collapse'> <tr style='height:15.5pt'><td width=250 colspan=2 valign=top style='width:187.5pt;border:none;border-top:solid #BED600 2.25pt;padding:0in 5.75pt 0in 0in;height:15.5pt'> <p class=MsoNormal style='line-height:115%'> <b> <span style='font-size:8.0pt;line-height:115%;font-family:\"Arial\",\"sans-serif\";color:#606060;mso-fareast-language:ZH-CN'>Very Efficient Law Firm LLP </span> </b> <b> <span style='font-size:8.0pt;line-height:115%;font-family:\"Arial\",\"sans-serif\";color:#606060;text-transform:uppercase;letter-spacing:1.0pt'> <br> </span> </b> <span style='font-size:8.0pt;margin-bottom:0pt;font-family:\"Arial\",\"sans-serif\";color:#606060;mso-fareast-language:ZH-CN'>123 Main Street, USA<o:p></o:p> </span> <b> <span style='font-size:8.0pt;line-height:115%;font-family:\"Arial\",\"sans-serif\";color:#606060;text-transform:uppercase;letter-spacing:1.0pt'> <br> </span> </b> <span style='font-size:8.0pt;margin-bottom:0pt;font-family:\"Arial\",\"sans-serif\";color:#606060;mso-fareast-language:ZH-CN'>800-555-2342<o:p></o:p> </span> </p> </td></tr> <tr><td width=39 valign=top style='width:28.9pt;padding:0in 5.75pt 0in 0in'></td></tr></table><p class=MsoNormal style='mso-margin-top-alt:3.0pt;margin-right:0in;margin-bottom:5.0pt;margin-left:0in;line-height:12.0pt;mso-line-height-rule:exactly'><i><span style='font-size:8.0pt;font-family:\"Arial\",\"sans-serif\";color:#606060;mso-fareast-language:ZH-CN'>Please consider the environment before printing this e-mail.<o:p></o:p></span></i></p><p class=MsoNormal><span style='font-size:8.0pt;font-family:\"Arial\",\"sans-serif\";color:#606060'><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><o:p>&nbsp;</o:p></p></div></body></html>"
+  "<br>Best,<br> Pat </body></html>" +  "<html><body lang=EN-US link=blue vlink=purple><div class=WordSection1><p class=MsoNormal style='margin-bottom:6.0pt'><b><span style='font-size:10.0pt;font-family:\"Arial\",\"sans-serif\";mso-fareast-language:ZH-CN'>Pat Lawyerton</span></b><b><span style='font-size:8.0pt;font-family:\"Arial\",\"sans-serif\";text-transform:uppercase;letter-spacing:1.0pt'><br></span></b><span style='font-size:8.0pt;font-family:\"Arial\",\"sans-serif\";color:#606060;mso-fareast-language:ZH-CN'>Senior Associate</span><span style='font-size:8.0pt;font-family:\"Arial\",\"sans-serif\"'><o:p></o:p></span></p><table class=MsoNormalTable border=0 cellspacing=0 cellpadding=0 style='border-collapse:collapse'> <tr style='height:15.5pt'><td width=250 colspan=2 valign=top style='width:187.5pt;border:none;border-top:solid #BED600 2.25pt;padding:0in 5.75pt 0in 0in;height:15.5pt'> <p class=MsoNormal style='line-height:115%'> <b> <span style='font-size:8.0pt;line-height:115%;font-family:\"Arial\",\"sans-serif\";color:#606060;mso-fareast-language:ZH-CN'>Very Efficient Law Firm LLP </span> </b> <b> <span style='font-size:8.0pt;line-height:115%;font-family:\"Arial\",\"sans-serif\";color:#606060;text-transform:uppercase;letter-spacing:1.0pt'> <br> </span> </b> <span style='font-size:8.0pt;margin-bottom:0pt;font-family:\"Arial\",\"sans-serif\";color:#606060;mso-fareast-language:ZH-CN'>123 Main Street, USA<o:p></o:p> </span> <b> <span style='font-size:8.0pt;line-height:115%;font-family:\"Arial\",\"sans-serif\";color:#606060;text-transform:uppercase;letter-spacing:1.0pt'> <br> </span> </b> <span style='font-size:8.0pt;margin-bottom:0pt;font-family:\"Arial\",\"sans-serif\";color:#606060;mso-fareast-language:ZH-CN'>800-555-2342<o:p></o:p> </span> </p> </td></tr> <tr><td width=39 valign=top style='width:28.9pt;padding:0in 5.75pt 0in 0in'></td></tr></table><p class=MsoNormal style='mso-margin-top-alt:3.0pt;margin-right:0in;margin-bottom:5.0pt;margin-left:0in;line-height:12.0pt;mso-line-height-rule:exactly'><i><span style='font-size:8.0pt;font-family:\"Arial\",\"sans-serif\";color:#606060;mso-fareast-language:ZH-CN'>Please consider the environment before printing this e-mail.<o:p></o:p></span></i></p><p class=MsoNormal><span style='font-size:8.0pt;font-family:\"Arial\",\"sans-serif\";color:#606060'><o:p>&nbsp;</o:p></span></p><p class=MsoNormal><o:p>&nbsp;</o:p></p></div></body></html>"
 
-  textbox.value = to+cc+subject + body;
+  var  textbox = to+cc+subject + body;
   var textFile = null,
-    makeTextFile = function (text) {
-      var data = new Blob([text], {type: 'text/plain'});
-      if (textFile !== null) {
-        window.URL.revokeObjectURL(textFile);}
-      textFile = window.URL.createObjectURL(data);
-      return textFile;};
-      var link = document.getElementById('downloadlink');
-      link.download = matter +" "+actionFull+".eml"
-      link.href = makeTextFile(textbox.value);
-      link.style.display = 'none';
-      document.getElementById("downloadlink").click(); // Click on the checkbox
-      // setTimeout(function(){window.location = window.location }, 1000);}
-
+  makeTextFile = function (text) {
+  var data = new Blob([text], {type: 'text/plain'});
+  if (textFile !== null) {
+    window.URL.revokeObjectURL(textFile);}
+  textFile = window.URL.createObjectURL(data);
+  return textFile;};
+  const newLink = document.createElement("a");
+  newLink.download = matter +" "+actionFull+".eml"
+  newLink.href = makeTextFile(textbox);
+  newLink.click();
 };
 
 function funcNfoaDraft(matter, clientno, attorney, title, file, ihc){
