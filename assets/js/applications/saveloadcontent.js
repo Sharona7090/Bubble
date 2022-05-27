40ecfbbd617d1987132e63f27c51053fbd4d0bc5
Sharona7090 <86896028+Sharona7090@users.noreply.github.com>
2022-05-27 17:46:46 -0500

    debug

--- assets/js/applications/saveloadcontent.js
+++ assets/js/applications/saveloadcontent.js
@@ -56,7 +56,7 @@ for (var i=0; i<loadApplication.savedFiguresArray.length; i++){
     var refnumber = loadApplication.savedFiguresArray[0][refidproperty]
     document.getElementById(`${x}_${k}`).value = refnumber
   }
-
+checkNumber()
   // if (loadApplication.savedFiguresArray[i].radio == "auto"){
   // document.getElementById('autoRef'+x).checked = true
   // }
