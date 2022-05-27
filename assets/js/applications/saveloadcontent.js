63d0bacf917396c007c29002e86b0daa62a4b435
Sharona7090 <86896028+Sharona7090@users.noreply.github.com>
2022-05-27 15:31:17 -0500

    debug

--- assets/js/applications/saveloadcontent.js
+++ assets/js/applications/saveloadcontent.js
@@ -23,7 +23,27 @@ function loadcontent(loadApplication){
   document.getElementById("claimsContainer").innerHTML = loadApplication.savedClaimsContainer;
   loadApplication.savedFiguresArray[0].radio.checked = true
 
+for (i=0; i<loadApplication.savedFiguresArray.length; i++){
+  var x = loadApplication.savedFiguresArray[i].$slide
+  var figInputID = "figinput"+x; //get figure number input ID
+  var figurenumber = document.getElementById(figInputID).value // get figure number value
+  var figoverlay = "figoverlay" +x; //get fig selected overlay ID
+  var pdfFigure = "pdfFigure" +x;
+  $("#"+figoverlay).css( "display", "initial" )
+  $("#"+figoverlay).text("FIG. " +figurenumber)
 
+var figtitle = "figtitle" +x;
+var spec = "spec"+x;
+$("#"+figInputID).addClass("numList");
+$("#"+figtitle).addClass("figList");
+$("#"+spec).addClass("specList");
+$("#"+spec).addClass("specList");
+
+$("#"+pdfFigure).addClass("selectedFig");
+$("#"+pdfFigure).attr('data-figid',x);
+$("#"+pdfFigure).attr('data-figno',figurenumber);
+
+}
 
 
 }
