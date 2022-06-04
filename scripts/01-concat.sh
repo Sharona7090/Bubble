56c22187a991454bc7f83aab71d8c8614ca89f50
Amanda Shih <pandafulmanda@gmail.com>
2022-06-04 15:18:35 -0500

    update build with echos

--- scripts/01-concat.sh
+++ scripts/01-concat.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "build application script"
 cat src/application.config.txt | xargs cat > dist/application.js
+echo "build email script"
 cat src/email.config.txt | xargs cat > dist/email.js
