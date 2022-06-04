67b1707e471e7db013cd8e154a9d611eeb9fca09
Amanda Shih <pandafulmanda@gmail.com>
2022-06-04 16:15:56 -0500

    scripts concat should have newline

--- scripts/01-concat.sh
+++ scripts/01-concat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "build application script"
-cat src/application.config.txt | xargs cat > dist/application.js
+cat src/application.config.txt | xargs -I{} sh -c "cat {}; echo ''" > dist/application.js
 echo "build email script"
-cat src/email.config.txt | xargs cat > dist/email.js
+cat src/email.config.txt | xargs -I{} sh -c "cat {}; echo ''" > dist/email.js
