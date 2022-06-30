ba844a2c4e33fa6dd09abe66842a2488cd5f3e86
Sharona7090 <86896028+Sharona7090@users.noreply.github.com>
2022-06-30 10:05:37 -0500

    db

--- assets/js/onboarding/onboarding.js
+++ assets/js/onboarding/onboarding.js
@@ -1,15 +1,42 @@
+
+$('link[rel=stylesheet][href~="https://sharona7090.github.io/Bubble/assets/css/style.bundle.css?v=7.0.4"]').remove();
+$('link[rel=stylesheet][href~="https://sharona7090.github.io/Bubble/assets/css/custom.css"]').remove();
+$('link[rel=stylesheet][href~="https://sharona7090.github.io/Bubble/assets/css/bubblecustom.css"]').remove();
+
+const tinyArray = []
+
+function tinymce_init_check(editorinput, uniqueid){
+  if (tinyArray.includes(uniqueid)){
+    initialize_tinymce(editorinput, uniqueid)
+    tinyArray.push(uniqueid)
+  }
+
+  // if (sessionStorage.getItem(uniqueid) ==""){
+  //   initialize_tinymce(editorinput, uniqueid)
+  //   sessionStorage.setItem(uniqueid, "yes");
+  // }
+
+}
+
+function initialize_tinymce(editorinput, uniqueid){
+
 tinymce.init({
  height : 400,
  selector: '.emaileditor',
-setup: function(editor) {
-       editor.on('blur', function(e) {
-        var content = tinymce.get(e.target.id).getContent();
-           alert(content);
-       });
-   },
-plugins: 'table lists',
-toolbar: 'undo redo fontfamily fontsize bold italic underline backcolor |outdent indent lineheight  bullist numlist table | alignleft aligncenter alignright alignjustify ',
+ setup: function(editor) {editor.on('init', function (e) {
+        editor.setContent(editorinput);
+        console.log("init fired")
+        });
+        editor.on('blur', function(e) {
+           var content = tinymce.get(e.target.id).getContent().replace(/(\r\n|\n|\r)/gm, '');
+
+           bubble_fn_tiny(content)
+         });
+       },
+ plugins: 'table lists',
+ toolbar: 'undo redo fontfamily fontsize bold italic underline fontcolor backcolor |outdent indent lineheight  bullist numlist table | alignleft aligncenter alignright alignjustify ',
  content_style: 'body { font-family:Arial,sans-serif; font-size:11pt }p{ margin:0}',
-font_size_formats: '8pt 9pt 10pt 11pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt',
+ font_size_formats: '8pt 9pt 10pt 11pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt'
 
 });
+}
