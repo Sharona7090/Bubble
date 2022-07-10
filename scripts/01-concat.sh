787bf1e60197032a3022de62a6b99e53ae876b53
Sharona7090 <86896028+Sharona7090@users.noreply.github.com>
2022-07-09 19:22:11 -0500

    create e_onboarding and i_onboarding

--- scripts/01-concat.sh
+++ scripts/01-concat.sh
@@ -7,3 +7,9 @@ cat src/email.config.txt | xargs -I{} sh -c "cat {}; echo ''" > dist/email.js
 
 echo "build prosecution script"
 cat src/prosecution.config.txt | xargs -I{} sh -c "cat {}; echo ''" > dist/prosecution.js
+
+echo "build e_onboarding script"
+cat src/e_onboarding.config.txt | xargs -I{} sh -c "cat {}; echo ''" > dist/e_onboarding.js
+
+echo "build i_onboarding script"
+cat src/i_onboarding.config.txt | xargs -I{} sh -c "cat {}; echo ''" > dist/i_onboarding.js
