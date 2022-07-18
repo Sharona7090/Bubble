3e98017ed3fb5ecdff07da4e428b0c6393e9399d
Sharona7090 <86896028+Sharona7090@users.noreply.github.com>
2022-07-18 11:44:36 -0500

    db

--- assets/js/e_onboarding/onboarding.js
+++ assets/js/e_onboarding/onboarding.js
@@ -38,10 +38,12 @@ function initialize_tinymce(editorinput, uniqueid){
          });
        },
        plugins: 'table lists',
-       menubar: 'edit format', 
+       menubar: 'edit format',
        toolbar: 'undo redo fontfamily fontsize bold italic underline fontcolor backcolor table |outdent indent lineheight  bullist numlist  | alignleft aligncenter alignright alignjustify ',
        content_style: 'body { font-family:Arial,sans-serif; font-size:11pt }p{ margin:0}',
-       font_size_formats: '8pt 9pt 10pt 11pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt'
+       font_size_formats: '8pt 9pt 10pt 11pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt',
+       table_default_attributes: {
+         cellpadding:'1px'}
     });
       console.log("after init code")
 }
